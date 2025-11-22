5e4448ceeec522b6577059c3ff99d4da610041b1
8972-amick <khadkaamick2@gmail.com>
2025-11-22 15:37:11 +0545

    Homepage started

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Bar from './Components/HomePage.jsx';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Bar />
+      
+    </div>
   )
 }
 
