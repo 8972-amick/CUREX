7958322c9fc328d384773b3dc2d709dcd15a0df4
8972-amick <khadkaamick2@gmail.com>
2025-11-25 13:38:55 +0545

    Rodri

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,14 @@
-const express = require('express');
+import express from "express"
+import "dotenv/config"
+import prisma from "./db/prisma.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;  
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+}); 
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Bar from './Components/HomePage.jsx';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <Bar />
-      
+      <Routes>
+        <Route path="/dashboard" element={<Bar />} />
+        <Route path="/history" element={<Bar />} />
+        <Route path="/contact" element={<Bar />} />
+        <Route path="/aboutus" element={<Bar />} />
+      </Routes>
     </div>
   )
 }
