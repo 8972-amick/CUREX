30ae09f7be6dfa9401897f14516530df44b2ef5b
8972-amick <khadkaamick2@gmail.com>
2025-11-29 12:51:09 +0545

    Login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Bar from './Components/HomePage.jsx';
+import LoginForm from './pages/login.jsx';
 import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/home" element={<Bar />} />
         <Route path="/dashboard" element={<Bar />} />
         <Route path="/history" element={<Bar />} />
         <Route path="/contact" element={<Bar />} />
---
+++ Frontend/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Bar from '../Components/HomePage'
+
+const home = () => {
+  return (
+    <>
+    </Bar>
+    </>
+    
+  )
+}
+
+export default home
