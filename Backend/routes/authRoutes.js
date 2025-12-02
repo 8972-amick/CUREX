addf5fd7cb689d829cae3fa134caf7253a6a19e7
8972-amick <khadkaamick2@gmail.com>
2025-12-02 21:21:38 +0545

    validation complete

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { signUp, logIn, logOut } from "../controllers/authControllers.js";
+import { signUpValidation, loginValidation } from "../middleware/authValidation.js";
 
 const authRouter = express.Router();
 
-authRouter.post('/signUp',signUp);
-authRouter.post('/logIn',logIn);
+authRouter.post('/signUp', signUpValidation, signUp);
+authRouter.post('/logIn', loginValidation, logIn);
 authRouter.post('/logOut',logOut);
 
 export default authRouter;
