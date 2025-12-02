00b41ea9b51e1397afd9e1e4135eb2fe5f512b4a
8972-amick <khadkaamick2@gmail.com>
2025-12-02 21:07:33 +0545

    authvalidation

--- Backend/controllers/authControllers.js
+++ Backend/controllers/authControllers.js
@@ -1,18 +1,22 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const user = await UserModel.findone({ email });
     if (user) {
-      return res.status(400).json({ message: "User already exists" });
+      return res
+        .status(400)
+        .json({ message: "User already exists", success: false });
     }
     const UserModel = new UserModel({ name, email, password });
     UserModel.password = await bcrypt.hash(password, 10);
     await UserModel.save();
-    res
-      .status(201)
-      .json({ message: "user created successfully", success: true });
+    res.status(201).json({
+      message: "user created successfully",
+      success: true,
+    });
   } catch (error) {
     res.status(500).json({
       message: "Internal server error",
@@ -22,18 +26,30 @@ export const signUp = async (req, res) => {
 };
 
 export const logIn = async (req, res) => {
-   try {
-    const { username, email, password } = req.body;
+  try {
+    const { email, password } = req.body;
     const user = await UserModel.findone({ email });
-    if (user) {
-      return res.status(400).json({ message: "User already exists" });
+    const errorMsg = "Authentication failed, Email or Password is Invalid";
+    if (!user) {
+      return res.status(403).json({ message: errorMsg, success: false });
     }
-    const UserModel = new UserModel({ name, email, password });
-    UserModel.password = await bcrypt.hash(password, 10);
-    await UserModel.save();
-    res
-      .status(201)
-      .json({ message: "user created successfully", success: true });
+    const isPasswordEqual = await bcrypt.compare(password, user.password);
+    if (!isPasswordEqual) {
+      return res.status(403).json({ message: errorMsg, success: false });
+    }
+    const jwtToken = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    res.status(200).json({
+      message: "Login Success",
+      success: true,
+      jwtToken,
+      email,
+      name: user.name,
+    });
   } catch (error) {
     res.status(500).json({
       message: "Internal server error",
@@ -41,6 +57,5 @@ export const logIn = async (req, res) => {
     });
   }
 };
-
 
 export const logOut = async (req, res) => {};
