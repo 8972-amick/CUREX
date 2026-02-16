379a53050ac0aaa402fdae5e39c1878e35ff0b59
8972-amick <khadkaamick2@gmail.com>
2026-02-16 16:17:40 +0545

    footer at homepage added

--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Footer from "../Components/footer.jsx";
+import Footer from "../components/footer";
+
 
 
 const Home = () => {
