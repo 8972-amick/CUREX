19e543e9cdcb6ef4ad66604913cf971db8e31a64
8972-amick <khadkaamick2@gmail.com>
2026-02-17 20:40:33 +0545

    working on the dashboard

---
+++ Frontend/src/pages/Notification.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold text-center mt-10">Notification Page</h1>
+        <p className="text-center mt-4 text-gray-600">
+          This is where the notification functionality will be implemented.
+        </p>
+        
+    </div>
+  )
+}
+
+export default Notification
