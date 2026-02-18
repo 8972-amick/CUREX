a7f7f19c9e5c5386b4b5a9991c6394e1d7c070d2
8972-amick <khadkaamick2@gmail.com>
2026-02-18 19:12:51 +0545

    Addition: Feature of license verification is on process

--- Frontend/src/pages/LicenseVerification.jsx
+++ Frontend/src/pages/LicenseVerification.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const LicenseVerification = () => {
   return (
-    <div>LicenseVerification</div>
+    <div>
+        <h1 className="text-3xl font-bold text-center mt-10">License Verification Page</h1>
+        <p className="text-center mt-4 text-gray-600">
+          This is where the license verification functionality will be implemented.
+        </p>
+        
+        
+    </div>
   )
 }
 
