32ae7ed77e4d4e48e2437bf598222c7d91c8a73b
8972-amick <khadkaamick2@gmail.com>
2026-02-20 20:34:48 +0545

    Addition: UI of patientDashboard being made

---
+++ Frontend/src/pages/patientDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const patientDashboard = () => {
+  return (
+    <div>
+        <h1>Patient Dashboard</h1>
+        <p>Welcome to your personalized patient dashboard.</p>
+    </div>
+  )
+}
+
+export default patientDashboard
