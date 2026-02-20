14a88e41f4a3798eeb3352c9d5c3b238edda641c
8972-amick <khadkaamick2@gmail.com>
2026-02-20 20:36:02 +0545

    Addition: UI of patientDashboard being made

--- Frontend/src/pages/patientDashboard.jsx
+++ Frontend/src/pages/patientDashboard.jsx
@@ -5,6 +5,12 @@ const patientDashboard = () => {
     <div>
         <h1>Patient Dashboard</h1>
         <p>Welcome to your personalized patient dashboard.</p>
+        <ul>
+            <li><a href="/my-appointments">My Appointments</a></li>
+            <li><a href="/symptoms">Symptom Analysis</a></li>
+            <li><a href="/chat">Chat with Doctor</a></li>
+            <li><a href="/notification">Notifications</a></li>
+        </ul>
     </div>
   )
 }
