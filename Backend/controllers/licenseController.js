fb34350fbf9be20ac7e9bc16d414fe7bf5117126
8972-amick <khadkaamick2@gmail.com>
2026-02-28 21:24:12 +0545

    feat:adminControllers added in the backend for verifying license

--- Backend/controllers/licenseController.js
+++
@@ -1,7 +0,0 @@
-
-export default function licenseController(req, res) {
-    res.json({ 
-        license: "This is a sample license text for the HealthMate application. All rights reserved." 
-    });
-    
-}
