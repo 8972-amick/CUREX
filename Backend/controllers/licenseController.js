d5b7bf8b4dfcfac969d985f902eaf89e32183f6d
8972-amick <khadkaamick2@gmail.com>
2026-02-28 21:16:12 +0545

    changes on the licenseController

---
+++ Backend/controllers/licenseController.js
@@ -0,0 +1,7 @@
+
+export default function licenseController(req, res) {
+    res.json({ 
+        license: "This is a sample license text for the HealthMate application. All rights reserved." 
+    });
+    
+}
