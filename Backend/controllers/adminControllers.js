4d2e0791ee9641f5a286c73aa847cbc95089feea
8972-amick <khadkaamick2@gmail.com>
2026-02-28 21:35:17 +0545

    feat:adminControllers added in the backend for verifying license

---
+++ Backend/controllers/adminControllers.js
@@ -0,0 +1,43 @@
+import prisma from "../db/prisma";
+
+//get all doctors
+export const getDoctors = async (req, res) => {
+  try {
+    const doctors = await prisma.user.findMany({
+      where: {
+        role: "Doctor",
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        licenseNumber: true,
+        isVerified: true,
+      },
+    });
+    res.json(doctors);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+
+  //verify the doctor
+  export const verifyDoctor = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const doctor = await prisma.user.update({
+        where: { id: parseInt(id) },
+        data: { isVerified: true },
+      });
+      res.json({
+        message: "Doctor verified successfully",
+        doctor,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "Server error",
+      });
+    }
+  };
+};
