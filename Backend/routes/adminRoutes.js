b12e2441e3fd5621d52840676827436db51cb4e3
8972-amick <khadkaamick2@gmail.com>
2026-03-01 19:31:31 +0545

    feat: route for admin added

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -3,3 +3,8 @@ import { getDoctors, verifyDoctor } from '../controllers/adminControllers';
 import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = express.Router();
+
+router.get('/doctors', authMiddleware, getDoctors);
+router.put('/doctors/:id/verify', authMiddleware, verifyDoctor);
+
+export default router;
