8a67c248cbc109ff1ca5b81f43af736ca542f0b7
8972-amick <khadkaamick2@gmail.com>
2026-03-05 18:16:09 +0545

    statCard.jsx updated

--- Frontend/src/Components/StatCard.jsx
+++ Frontend/src/Components/StatCard.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const StatCard = () => {
+const StatCard = ({ label, value, icon: Icon, color }) => {
   return (
-    <div>
-        <div className='statCard'>
-            <div className='statCardTitle'>Total Sales</div>
-            <div className='statCardValue'>$12,345</div>
-        </div>
-    </div>
-  )
-}
+    <div className="bg-white rounded-lg p-4 shadow-sm flex items-center gap-4 hover:shadow-md transition">
+      
+      <div className={`p-3 rounded-md ${color}`}>
+        <Icon className="w-6 h-6" />
+      </div>
 
-export default StatCard
+      <div>
+        <p className="text-sm text-gray-500">{label}</p>
+        <h3 className="text-2xl font-semibold">{value}</h3>
+      </div>
+
+    </div>
+  );
+};
+
+export default StatCard;
