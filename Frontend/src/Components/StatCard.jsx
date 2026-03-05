ef4ecd9307612c9c64229a3990e9aba68a9e0b16
8972-amick <khadkaamick2@gmail.com>
2026-03-05 18:13:49 +0545

    statCard.jsx updated

--- Frontend/src/Components/StatCard.jsx
+++ Frontend/src/Components/StatCard.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const StatCard = () => {
   return (
-    <div>StatCard</div>
+    <div>
+        <div className='statCard'>
+            <div className='statCardTitle'>Total Sales</div>
+            <div className='statCardValue'>$12,345</div>
+        </div>
+    </div>
   )
 }
 
