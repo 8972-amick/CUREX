47d1d958242ce34bb37bdf0f38b7db357e99422a
8972-amick <khadkaamick2@gmail.com>
2026-03-06 18:46:33 +0545

    verify doctor added to the adminAPI.js file

--- Frontend/src/services/adminApi.js
+++ Frontend/src/services/adminApi.js
@@ -1,17 +1,25 @@
 import axios from "axios";
 
 const API = "http://localhost:3000/api/admin";
-export const getDoctors = (token) => {
-    axios.get(`{API}/doctors`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        }
-    });
 
-    export const  verifyDoctor = (id, token) =>{
-    axios.put(`${API}/doctors/${id}/verify`, {}, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        }
-    }); 
-}
+// Get all doctors
+export const getDoctors = (token) => {
+  return axios.get(`${API}/doctors`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+// Verify a doctor
+export const verifyDoctor = (id, token) => {
+  return axios.put(
+    `${API}/verify/${id}`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
